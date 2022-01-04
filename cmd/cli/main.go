f98baf95a5eb033ac79a3120e0175b32b6fdc563
Menno <menno.vrahden@gmail.com>
2022-01-04 23:33:57 +0100

    return error an fail in main instead

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,7 +3,6 @@ package cli
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,26 +16,34 @@ var (
 	scanPath string
 )
 
+const (
+	ArgumentKeyTypeAlias         = "typealias"
+	ArgumentKeySupport           = "support"
+	ArgumentKeySerializers       = "serializers"
+	ArgumentKeyTransformStrategy = "transform"
+	ArgumentKeyScanDirectory     = "dir"
+)
+
 func init() {
 	// flag.StringVar(&args.Output, "output", "", "the filename of the generated file; defaults to \"<typealias|snake>_enumer.go\".")
 	// flag.StringVar(&args.AddPrefix, "addprefix", "", "add given prefix to string values of enum.")
-	flag.StringVar(&args.TransformStrategy, "transform", "noop", "string transformation (camel|kebab|pascal|snake|upper-kebab|upper-snake|whitespace); defaults to \"noop\" which applies no transormation to the enum value.")
-	flag.StringVar(&args.TypeAliasName, "typealias", "", "the type alias (or type name) to perform the scan against.")
-	flag.Var(&args.Serializers, "serializers", "a list of opt-in serializers (binary|json|sql|text|yaml).")
-	flag.Var(&args.SupportedFeatures, "support", "a list of opt-in supported features (undefined|ent).")
-	flag.StringVar(&scanPath, "dir", "", "directory of target package; defaults to CWD.")
+	flag.StringVar(&args.TransformStrategy, ArgumentKeyTransformStrategy, "noop", "string transformation (camel|pascal|kebab|snake|... see README.md); defaults to \"noop\" which applies no transormation to the enum values.")
+	flag.StringVar(&args.TypeAliasName, ArgumentKeyTypeAlias, "", "the type alias (or type name) to perform the scan against.")
+	flag.Var(&args.Serializers, ArgumentKeySerializers, "a list of opt-in serializers (binary|json|sql|text|yaml).")
+	flag.Var(&args.SupportedFeatures, ArgumentKeySupport, "a list of opt-in supported features (undefined|ent).")
+	flag.StringVar(&scanPath, ArgumentKeyScanDirectory, "", "directory of target package; defaults to CWD.")
 }
 
 type Generator interface {
 	Generate(targetPkg string) ([]byte, error)
 }
 
-func Execute() {
+func Execute() error {
 	flag.Parse()
 	cfg := config.LoadWith(&args)
 
-	if cfg.Serializers.Contains("yaml") && cfg.Serializers.Contains("yaml.v3") {
-		log.Fatalf("serializers %q and %q are cannot be applied together.", "yaml", "yaml.v3")
+	if err := validate(cfg); err != nil {
+		return fmt.Errorf("invalid arguments. err: %s", err)
 	}
 
 	targetDir, _ := os.Getwd()
@@ -44,28 +51,39 @@ func Execute() {
 		targetDir = filepath.Clean(scanPath)
 	}
 
-	f, err := os.Create(targetFilename(targetDir, cfg))
-	if err != nil {
-		log.Fatalf("failed opening %q. err: %s", targetFilename(targetDir, cfg), err)
-	}
-	defer f.Close()
-
 	g := gen.NewGenerator(
 		gen.NewInspector(cfg),
 		gen.NewRenderer(cfg),
 	)
 	buf, err := g.Generate(targetDir)
 	if err != nil {
-		log.Fatalf("failed generating code. err: %s", err)
+		return fmt.Errorf("failed generating code. err: %s", err)
 	}
+
+	f, err := os.Create(targetFilename(targetDir, cfg))
+	if err != nil {
+		return fmt.Errorf("failed opening %q. err: %s", targetFilename(targetDir, cfg), err)
+	}
+	defer f.Close()
 
 	_, err = f.Write(buf)
 	if err != nil {
-		log.Fatalf("failed writing output to file. err: %s", err)
+		return fmt.Errorf("failed writing output to file. err: %s", err)
 	}
+	return nil
 }
 
 func targetFilename(dir string, cfg *config.Options) string {
 	filename := fmt.Sprintf("%s_enumer.go", strings.ToLower(cfg.TypeAliasName))
 	return filepath.Join(dir, filename)
+}
+
+func validate(cfg *config.Options) error {
+	if len(cfg.TypeAliasName) == 0 {
+		return fmt.Errorf("argument %q cannot be empty.", ArgumentKeyTypeAlias)
+	}
+	if cfg.Serializers.Contains("yaml") && cfg.Serializers.Contains("yaml.v3") {
+		return fmt.Errorf("serializers %q and %q are cannot be applied together.", "yaml", "yaml.v3")
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mvrahden/go-enumer/cmd/cli"
 )
 
 func main() {
-	cli.Execute()
+	err := cli.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
